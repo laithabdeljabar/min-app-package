61955f71676352a81237e1c909695d8f3a35ed3e
Laith Abdeljabar <laith@gmail.com>
2025-11-23 13:15:40 +0300

    push all files and add view

--- Sources/min app package/min_app_package.swift
+++ Sources/min app package/min_app_package.swift
@@ -1,2 +1,28 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftUI
+
+public struct MySwiftModule {
+    public static func getVersion() -> String {
+        return "1.0.0"
+    }
+
+    public static func processData(params: [String: Any]) -> String {
+        return "Processed: \(params)"
+    }
+}
+
+public struct MySwiftUIScreen: View {
+    let text: String
+
+    public init(text: String) {
+        self.text = text
+    }
+
+    public var body: some View {
+        VStack {
+            Text("SwiftUI Screen")
+            Text(text)
+        }
+        .padding()
+    }
+}
